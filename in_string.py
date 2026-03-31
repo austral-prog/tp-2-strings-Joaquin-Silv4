e902209f3c1f4a39fb92cddcc1ea8c8f51385b15
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-03-31 12:36:06 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -3,3 +3,17 @@ def check_vowels():
     sin distinguir mayúsculas de minúsculas.
     """
     pass
+    nombre=input("ingrese nombre:").lower()
+    tiene_a="a" in nombre
+    tiene_e="e" in nombre
+    tiene_i="i" in nombre
+    tiene_o="o" in nombre
+    tiene_u="u" in nombre
+
+    print(f"¿Tiene 'a'?: {tiene_a}")
+    print(f"¿Tiene 'e'?: {tiene_e}")
+    print(f"¿Tiene 'i'?: {tiene_i}")
+    print(f"¿Tiene 'o'?: {tiene_o}")
+    print(f"¿Tiene 'u'?: {tiene_u}")
+
+check_vowels()
