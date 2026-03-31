e548db549ac0a84aa62e0f265dd2972d9b403464
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-03-31 12:37:49 -0300

    Update string_info.py

--- string_info.py
+++ string_info.py
@@ -3,3 +3,19 @@ def string_info():
     la palabra repetida 3 veces y decorada con '***'.
     """
     palabra = "Programacion"
+
+    print(palabra[:])
+
+    longitud=len(palabra)
+    print(longitud)
+
+    print(palabra[0])
+
+    print(palabra[-1])
+
+    print(palabra[:]*3)
+
+    print(f"***{palabra}***")
+
+
+string_info()
