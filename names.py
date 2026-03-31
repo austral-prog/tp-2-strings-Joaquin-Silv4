3d4b37f4cad9822e1e448d21685cb6333568bbed
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-03-31 12:34:11 -0300

    Update names.py

--- names.py
+++ names.py
@@ -3,3 +3,12 @@ def names():
     minúsculas, título, mayúsculas y con tabulador.
     """
     pass
+    nombre=input("ingrese nombre:")
+    apellido=input("ingrese apellido:")
+    nombre_completo=nombre+"  "+apellido
+    print(nombre_completo.lower())          #todo minusculas
+    print(nombre_completo.title())          #con mayusculas al inicio
+    print(nombre_completo.upper())          #todo mayusculas
+    print(f"\t{nombre_completo}".lower())   #con tabulacion
+
+names()
