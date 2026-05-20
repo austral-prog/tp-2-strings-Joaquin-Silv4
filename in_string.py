45fe7747afef6ff0a01a76f5d002eec54cb116ab
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-05-20 20:05:17 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -2,18 +2,10 @@ def check_vowels():
     """Lee un nombre y verifica si contiene cada una de las vocales (a, e, i, o, u),
     sin distinguir mayúsculas de minúsculas.
     """
-    pass
-    nombre=input("ingrese nombre:").lower()
-    tiene_a="a" in nombre
-    tiene_e="e" in nombre
-    tiene_i="i" in nombre
-    tiene_o="o" in nombre
-    tiene_u="u" in nombre
+    nombre = input().lower()
 
-    print(f"¿Tiene 'a'?: {tiene_a}")
-    print(f"¿Tiene 'e'?: {tiene_e}")
-    print(f"¿Tiene 'i'?: {tiene_i}")
-    print(f"¿Tiene 'o'?: {tiene_o}")
-    print(f"¿Tiene 'u'?: {tiene_u}")
-
-check_vowels()
+    print(f"Contiene a: {'a' in nombre}")
+    print(f"Contiene e: {'e' in nombre}")
+    print(f"Contiene i: {'i' in nombre}")
+    print(f"Contiene o: {'o' in nombre}")
+    print(f"Contiene u: {'u' in nombre}")
